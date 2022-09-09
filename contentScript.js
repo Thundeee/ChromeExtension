df32fd1d870e8666ff7e0602c652a769c85bd973
Asbjørn <78221203+Thundeee@users.noreply.github.com>
2022-09-09 17:55:51 +0200

    first real commit, only works 1 time

--- contentScript.js
+++ contentScript.js
@@ -1,40 +1,61 @@
-// background.js
+let pkmn = "";
+let username = "";
 
-/*let pkmn = '';
+setTimeout(coreExtension, 5000);
 
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.sync.set({ color });
-});
-*/
-
-
-var wakeup = function(){
-  setTimeout(function(){
-      chrome.runtime.sendMessage('ping', function(response){
-          console.log(response);
-      });
-      wakeup();
-  }, 1000);
+function coreExtension() {
+  usernameGetter();
+  observerMain();
 }
-wakeup();
 
+function usernameGetter() {
+  username = document.querySelector(".username").getAttribute("data-name");
+  console.log(username);
+  return username;
+}
 
-function checkPkmn() {
-
-
-    let test = window.getElementById("battle-history");
-  console.log(test);
-  /*if (test.includes("sent out")) {
-  console.log(test);  
-  }
-  else console.log("zz");*/
-  }
-  
-  //  console.log('Enemy used pokemon:', `${pkmn}`);
-  
-  checkPkmn();
-
-
-  chrome.runtime.sendMessage({greeting: "hello"}, function(response) {
-    console.log(response.farewell);
+function observerMain() {
+  const mutationObserver = new MutationObserver((entries) => {
+    console.log(entries);
+    sendMsg(entries);
   });
+
+  const parent = document.querySelector(".battle-log");
+  const correctChild = parent.querySelector(".message-log");
+
+  const observer = new MutationObserver(function (mutations_list) {
+    mutations_list.forEach(function (mutation) {
+      mutation.addedNodes.forEach(function (added_node) {
+        if (
+          mutation.addedNodes[0].classList.contains("battle-history") &&
+          mutation.addedNodes[0].innerHTML.includes("sent out")
+        ) {
+          if (mutation.addedNodes[0].innerHTML.includes(username)) {
+            return;
+          } else {
+            console.log(mutation.addedNodes[0].innerHTML);
+
+            result = mutation.addedNodes[0].innerHTML;
+            pkmn = result.match(/\>(.*?)\</);
+            console.log(pkmn[1]);
+
+            const parentText = document.querySelector(".battle-log-add");
+            const chatBox = parentText.querySelectorAll(".textbox")[1];
+
+            chatBox.innerHTML = "/weak " + pkmn[1];
+
+            console.log("enemy pokemon");
+            console.log(mutation.addedNodes[0]);
+          }
+        } else {
+          console.log("irrelevant node");
+        }
+      });
+    });
+  });
+
+  observer.observe(parent.querySelector(".message-log"), {
+    subtree: false,
+    childList: true,
+  });
+}
